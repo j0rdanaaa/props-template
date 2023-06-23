a97b9043c28075be6a8a3b29c59bacdcee75c3dd
j0rdanaaa <j0rdanaaa@gmail.com>
2023-06-23 16:25:56 -0300

    template props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,65 @@
+import { FOCUSABLE_SELECTOR } from "@testing-library/user-event/dist/utils";
 import Garagem from "./Componentes/Garagem";
 import "./styles.css";
 
 export default function App() {
+  const nome = 'Jordana'
+  const sobrenome = ' Góes'
+
+  const apresentaGaragem = (nome) => {
+alert('Boas vindas a garagem de ' + nome)
+//alert(`Boas vindas a garagem de ${nome}`)
+  }
+
+  const nome2 = 'Jordana'
+
+  /*const dadosCarro = {
+    nome: 'Fusca',
+    ano: 1979,
+    cor: 'Amarelo',
+    flex: false
+  }*/
+
+    const dadosCarro2 = {
+    nome: 'Fusca',
+    ano: 1979,
+    cor: 'Vermelho',
+    flex: false
+    }
+    const dadosCarro3 = {
+    nome: 'Fusca',
+    ano: 1972,
+    cor: 'Preto',
+    flex: false
+    }
+    const dadosCarro4 = {
+    nome: 'Fusca',
+    ano: 1970,
+    cor: 'Azul',
+    flex: false
+    }
+    const dadosCarro5 = {
+    nome: 'Fusca',
+    ano: 1977,
+    cor: 'Branco',
+    flex: false
+    }
+    
   return (
-    <div>
-      <Garagem />
+      <div className="App">
+        {/* <Garagem
+        nome={nome}
+        sobrenome={sobrenome}
+        apresentaGaragem={apresentaGaragem}
+        dadosCarro={dadosCarro}
+      /> */}
+       <Garagem nome={nome2}
+       apresentaGaragem={apresentaGaragem}
+       dadosCarro2={dadosCarro2}
+       dadosCarro3={dadosCarro3}
+       dadosCarro4={dadosCarro4}
+       dadosCarro5={dadosCarro5}
+       />
     </div>
   );
-}
+ }
--- src/Componentes/Garagem.js
+++ src/Componentes/Garagem.js
@@ -1,13 +1,24 @@
 import Carro from "./Carro";
 
-function Garagem() {
+function Garagem({ nome, sobrenome, apresentaGaragem, dadosCarro2, dadosCarro3, dadosCarro4, dadosCarro5 }) {
+  // const { nome, sobrenome, apresentaGaragem } = props
+
   return (
     <div>
-      <h1>Garagem da Labenu</h1>
-      <Carro />
-      <Carro />
-      <Carro />
-      <Carro />
+      <h1>Garagem de {nome + sobrenome}</h1>
+      <Carro dadosCarro={dadosCarro2} />
+      <Carro dadosCarro={dadosCarro3} />
+      <Carro dadosCarro={dadosCarro4} />
+      <Carro dadosCarro={dadosCarro5} />
+    
+
+      <button
+        onClick={() => {
+          apresentaGaragem(nome);
+        }}
+      >
+        Botao
+      </button>
     </div>
   );
 }
